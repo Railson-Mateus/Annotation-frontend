7bae1d243d4afbc3b0d8d7c0ca3c0018a89a6d34
Railson Mateus <raylsomatheus@hotmail.com>
2023-01-27 10:21:18 -0300

    Cria service annotation e user

--- src/services/annotationService.js
+++ src/services/annotationService.js
@@ -1,9 +1,47 @@
+import { Api } from "./api";
+
 class AnnotationService {
-  static async create(annotation) {}
-  static async update(data) {}
+  static async create(annotation) {
+    try {
+      const result = await Api.post(annotation);
+
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async update(data) {
+    try {
+      const result = await Api.patch(data);
+
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+
   static async getAnnotationById(id) {}
-  static async list(userId) {}
-  static async delete(id) {}
+
+  static async list(userId) {
+    try {
+      const result = await Api.get("/annotations");
+
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async delete(id) {
+    try {
+      const result = await Api.delete(`/annotations/${id}`);
+
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 export default AnnotationService;
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,25 @@
 import { Api } from "./api";
 
 class UserService {
-  static async create(user) {}
-  static async update(user) {}
+  static async create(user) {
+    try {
+      const request = await Api.post("/user", user);
+
+      return request.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  static async update(user) {
+    try {
+      const request = await Api.patch(`/user`, user);
+
+      return request.data;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 export default UserService;
