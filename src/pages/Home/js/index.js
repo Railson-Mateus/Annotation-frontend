d0d7c879316de49e0ac99a7e1a1078337f574bad
Italo souza sampaio <italo12346@gmail.com>
2023-01-28 17:17:02 -0300

    estruturando a pagina

---
+++ src/pages/Home/js/index.js
@@ -0,0 +1,18 @@
+const data = new Date()
+const dia = String(data.getDate()).padStart(2, '0')
+const mes = String(data.getMonth() + 1).padStart(2, '0')
+const ano = data.getFullYear()
+const dataAtual = `${dia}/${mes}/${ano}`
+const hoje = document.querySelector('.data')
+const usuario = document.querySelector('.nome')
+
+let nomeUsuario = "Italo Souza Sampaio" //vai receber da tela de login 
+
+function infoUser(hoje,usuario,nomeUsuario){
+    hoje.textContent = dataAtual 
+    usuario.textContent = nomeUsuario
+}
+infoUser(hoje,usuario,nomeUsuario)
+
+
+
