f476a32ccafc40522fa716d62c4fa25e50cdf1e3
Railson Mateus <raylsomatheus@hotmail.com>
2023-01-30 15:30:28 -0300

    cria component card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,24 +1,22 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
 
-export default function Card(props) {
-    return (
-        
-        <div className="posts">
-            <div className="titulo">
-                <h2>{props.a}</h2>
-                <p>08:30</p>
-            </div>
-            <div className="assunto">
-              <p>Assunto</p>
-
-            </div>
-            <div className="rodape">
-                <div className="data">data
-                <p>data</p>
-                </div>
-                <input type="checkbox" name="" id="checkbox"/>
-            </div>
+export function Card(props) {
+  return (
+    <div className="card">
+      <div className="titulo">
+        <h2>{props.value}</h2>
+        <p>08:30</p>
+      </div>
+      <div className="assunto">
+        <p>Assunto</p>
+      </div>
+      <div className="rodape">
+        <div className="data">
+          <p>data</p>
         </div>
-    )
+        <input type="checkbox" name="" id="checkbox" checked />
+      </div>
+    </div>
+  );
 }
